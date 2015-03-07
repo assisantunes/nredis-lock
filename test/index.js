f9a2ac16481fc5fb4ae085ad83f77f5fdd58d242
Assis Silva <assis@hackhands.com>
2015-03-07 13:32:48 -0800

    Allow release a lock without acquiring it first;

--- test/index.js
+++ test/index.js
@@ -18,63 +18,104 @@ describe('Lock', function(){
     it('should lock', function(done){
         this.timeout(3000);
 
-        Lock('test-1').then(function(release){
-            assert(true);
+        Lock('test-1')
+            .acquire()
+            .then(function(release){
+                assert(true);
 
-            setTimeout(function(){
-                release();
-            }, 1000);
-        });
+                setTimeout(function(){
+                    release();
+                }, 1000);
+            });
 
-        Lock('test-1').then(null, function(){
-            assert(true);
-            done();
-        });
+        Lock('test-1')
+            .acquire()
+            .then(null, function(){
+                assert(true);
+                done();
+            });
     });
 
     it('should wait until release the last lock', function(done){
         this.timeout(3000);
 
         // Lock using 1000 as timeout
-        Lock('test-2', 1000, function(){
-            assert(true);
-        });
+        Lock('test-2', 1000)
+            .acquire()
+            .then(function(){
+                assert(true);
+            }, function(){
+                assert(false, 'should acquire the lock');
+            });
 
         // should be locked
-        Lock('test-2').then(null, function(){
-            assert(true);
-        });
+        Lock('test-2')
+            .acquire()
+            .then(function(){
+                assert(false, 'should not acquire the lock');
+            }, function(){
+                assert(true);
+            });
 
         // Running after the first lock be released
-        Lock('test-2', null, true).then(function(release){
-            assert(true);
-            release();
-            done();
-        });
-
+        Lock('test-2', null, true)
+            .acquire()
+            .then(function(release){
+                done();
+                release();
+            });
     });
 
     it('lock should be release any time', function(done){
         this.timeout(4000);
 
+        var mylock = Lock('test-3');
+
         // Acquiring the lock
-        var mylock = Lock('test-3').then(function(){
-            assert(true, 'should acquire the lock');
+        mylock.acquire()
+            .then(function(){
+                assert(true, 'should acquire the lock');
 
-            // Release the lock
-            mylock.release().then(function(){
-                assert(true, 'lock released');
+                // Release the lock
+                mylock.release().then(function(){
+                    assert(true, 'lock released');
 
-                Lock('test-3').then(function(release){
-                    assert(true, 'should acquire the lock after force release');
+                    Lock('test-3')
+                        .acquire()
+                        .then(function(release){
+                            assert(true, 'should acquire the lock after force release');
 
-                    done();
+                            done();
+                        }, function(){
+                            assert(false, 'the lock should be released');
+
+                            done();
+                        });
                 }, function(){
-                    assert(false, 'the lock should be released');
+                    assert(false, 'should have lock to release');
 
                     done();
                 });
+            }, function(){
+                assert(false, 'should acquire the lock');
+
+                done();
             });
-        });
+    });
+
+    it('test no lock to be released', function(done){
+        this.timeout(4000);
+
+        Lock('test-4')
+            .release()
+            .then(function(){
+                assert(false, 'should have no lock to be released');
+
+                done();
+            }, function(){
+                assert(true, 'no lock to be released');
+
+                done();
+            });
     });
 });
